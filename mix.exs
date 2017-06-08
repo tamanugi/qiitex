260819d56ee9d7c39d7dad85f2a408d953294cc8
tamanugi <tamanugi@gmail.com>
2017-06-08 22:03:19 +0900

    add deps httposison and poison

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule Qiitex.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [applications: [:httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +28,9 @@ defmodule Qiitex.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~>0.11"},
+      {:poison, "~>3.1"}
+    ]
   end
 end
