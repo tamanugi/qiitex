a1d377406c0982a25c6c7b55dd449bee2436795e
tamanugi <tamanugi@gmail.com>
2017-06-18 16:53:41 +0900

    followers_count and items_count don't check value

--- test/tags_test.exs
+++ test/tags_test.exs
@@ -20,8 +20,10 @@ defmodule Qiitex.TagsTest do
     test "find/2" do
         tag = find(@client, "elixir")
         assert ExJsonSchema.Validator.validate(SchemaHelper.schema("tag") , tag) == :ok
-        assert tag == %{"followers_count" => 902,
-                       "icon_url" => "https://s3-ap-northeast-1.amazonaws.com/qiita-tag-image/6e0b49c8d70cd06c57386d923a8362fbaf71c233/medium.jpg?1364840830",
-                       "id" => "Elixir", "items_count" => 883}
+        %{"icon_url" => icon_url,
+          "id" => id} = tag
+
+        assert icon_url  == "https://s3-ap-northeast-1.amazonaws.com/qiita-tag-image/6e0b49c8d70cd06c57386d923a8362fbaf71c233/medium.jpg?1364840830"
+        assert id == "Elixir"
     end
 end
