3ecd08f2f6e34257c7236303ceae74c1745737b3
tamanugi <tamanugi@gmail.com>
2017-06-18 16:39:52 +0900

    add test fot Tags.find/2

--- test/tags_test.exs
+++ test/tags_test.exs
@@ -16,4 +16,12 @@ defmodule Qiitex.TagsTest do
           assert ExJsonSchema.Validator.validate(SchemaHelper.schema("tag") , e) == :ok
         end)
     end
+
+    test "find/2" do
+        tag = find(@client, "elixir")
+        assert ExJsonSchema.Validator.validate(SchemaHelper.schema("tag") , tag) == :ok
+        assert tag == %{"followers_count" => 902,
+                       "icon_url" => "https://s3-ap-northeast-1.amazonaws.com/qiita-tag-image/6e0b49c8d70cd06c57386d923a8362fbaf71c233/medium.jpg?1364840830",
+                       "id" => "Elixir", "items_count" => 883}
+    end
 end
