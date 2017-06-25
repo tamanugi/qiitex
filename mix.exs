2d2d7adb3da5679cd24ff97914366315eaae9791
tamanugi <tamanugi@gmail.com>
2017-06-25 23:24:12 +0900

    add metainfo

--- mix.exs
+++ mix.exs
@@ -5,6 +5,12 @@ defmodule Qiitex.Mixfile do
     [app: :qiitex,
      version: "0.1.0",
      elixir: "~> 1.4",
+     description: "Simple API wrapper for Qiita API v2",
+     package: [
+       maintainers: ["tamanugi"],
+       licenses: ["MIT"],
+       links: %{"GitHub" => "https://github.com/tamanugi/qiitex"}
+     ],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
