e405229547f7fd93960c490accc49d4ff95a35fc
tamanugi <tamanugi@gmail.com>
2017-07-10 22:23:38 +0900

    add delete comment

--- lib/qiitex/comments.ex
+++ lib/qiitex/comments.ex
@@ -8,4 +8,8 @@ defmodule Qiitex.Comments do
   def list_item_comments(client, item_id \\ "") do
     get "/items/#{item_id}/comments", client
   end
+
+  def delete_comment(client, comment_id) do
+    delete "/comments/#{comment_id}", client
+  end
 end
