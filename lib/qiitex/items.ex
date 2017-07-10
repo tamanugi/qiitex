beaf187e882625479f22ab0367d874d84d6f1d9e
tamanugi <tamanugi@gmail.com>
2017-07-10 23:36:36 +0900

    add items api

--- lib/qiitex/items.ex
+++ lib/qiitex/items.ex
@@ -20,4 +20,33 @@ defmodule Qiitex.Items do
   def list_user_stock_items(client, user_id \\ "", params \\ []) do
     get "/users/#{user_id}/stocks", client, params
   end
+
+  def list_authenticated_user_items(client, params \\ []) do
+    get "/authenticated_user/items", client, params
+  end
+
+  def create(client, params) do
+    post "/items", client, params
+  end
+
+  def delete_item(client, item_id) do
+    delete "/items/#{item_id}", client
+  end
+
+  def update(client, item_id, params) do
+    patch "/items/#{item_id}", client, params
+  end
+
+  def stock(client, item_id) do
+    put "/items/#{item_id}/stock", client
+  end
+
+  def unstock(client, item_id) do
+    delete "/items/#{item_id}/stock", client
+  end
+
+  def get_stock(client, item_id) do
+    get "/items/#{item_id}/stock", client
+  end
+
 end
