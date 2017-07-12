cfd30a334a462348ff0a3c06e918a8abb6bcf9d1
tamanugi <tamanugi@gmail.com>
2017-07-12 09:45:34 +0900

    add function docs

--- lib/qiitex/items.ex
+++ lib/qiitex/items.ex
@@ -1,50 +1,176 @@
 defmodule Qiitex.Items do
   import Qiitex
 
+  @doc """
+  List items.
+
+  ## Example
+    
+    ```
+    Qiitex.Items.list client
+    Qiitex.Items.list client, %{page: 1, per_page: 40, query: "qiita"}
+    ```
+    
+  """
   def list(client, params \\ []) do
     get "/items", client, params
   end
 
+  @doc """
+  Get an Item.
+
+  ## Example
+    
+    ```
+    Qiitex.Item.find client, "itemid"
+    ```
+    
+  """
   def find(client, item_id \\ "") do
     get "/items/#{item_id}", client
   end
 
+  @doc """
+  List tagged items in recently-tagged order.
+
+
+  ## Example
+    
+    ```
+    Qiitex.Items.list_tag_items client, "tag_id"
+    Qiitex.Items.list_tag_items client, "tag_id", %{page: 1, per_page: 40}
+    ```
+    
+  """
   def list_tag_items(client, tag_id \\ "", params \\ []) do
     get "/tags/#{tag_id}/items", client, params
   end
 
+  @doc """
+  List a user's items in newest order.
+
+  ## Example
+    
+    ```
+    Qiitex.Items.list_user_items client, "user_id"
+    Qiitex.Items.list_user_items client, "user_id", %{page: 1, per_page: 40}
+    ```
+    
+  """
   def list_user_items(client, user_id \\ "", params \\ []) do
     get "/users/#{user_id}/items", client, params
   end
 
+  @doc """
+  List a user's stocked items in recently-stocked order.
+
+  ## Example
+    
+    ```
+    Qiitex.Items.list_user_stock_items client, "user_id"
+    Qiitex.Items.list_user_stock_items client, "user_id", %{page: 1, per_page: 40}
+    ```
+    
+  """
   def list_user_stock_items(client, user_id \\ "", params \\ []) do
     get "/users/#{user_id}/stocks", client, params
   end
 
+  @doc """
+  List the authenticated user's items in newest order
+
+  ## Example
+    
+    ```
+    Qiitex.Items.list_authenticated_user_items client
+    Qiitex.Items.list_authenticated_user_items client, %{page: 2, per_page: 40}
+    ```
+    
+  """
   def list_authenticated_user_items(client, params \\ []) do
     get "/authenticated_user/items", client, params
   end
 
+  @doc """
+  Create an item.
+
+  ## Example
+    
+    ```
+    Qiitex.Items.create client, %{title: "title", body: "body", tags: [%{name: "qiita"}]}
+    ```
+    
+  """
   def create(client, params) do
     post "/items", client, params
   end
 
+  @doc """
+  Delete an item.
+
+  ## Example
+    
+    ```
+    Qiitex.Item.delete_item client, "itemid"
+    ```
+    
+  """
   def delete_item(client, item_id) do
     delete "/items/#{item_id}", client
   end
 
+  @doc """
+  Update an item.
+
+  ## Example
+    
+    ```
+    Qiitex.Items.update client, "itemid", %{title: "title", body: "body", tags: [%{name: "qiita"}]}
+    ```
+    
+  """
   def update(client, item_id, params) do
     patch "/items/#{item_id}", client, params
   end
 
+  @doc """
+  Stock an item.
+
+  ## Example
+    
+    ```
+    Qiitex.Items.stock client, "itemid"
+    ```
+    
+  """
   def stock(client, item_id) do
     put "/items/#{item_id}/stock", client
   end
 
+  @doc """
+  Unstock an item.
+
+  ## Example
+    
+    ```
+    Qiitex.Items.unstock client, "itemid"
+    ```
+    
+  """
   def unstock(client, item_id) do
     delete "/items/#{item_id}/stock", client
   end
 
+  @doc """
+  Check if you stocked an item.
+
+  ## Example
+    
+    ```
+    Qiitex.Items.get_stock client, "itemid"
+    ```
+    
+  """
   def get_stock(client, item_id) do
     get "/items/#{item_id}/stock", client
   end
--- lib/qiitex/users.ex
+++ lib/qiitex/users.ex
@@ -5,34 +5,110 @@ defmodule Qiitex.User do
     get "/authenticated_user", client
   end
 
+  @doc """
+  List users who stocked an item in recent-stocked order.
+
+  ## example
+
+    ```
+    Qiitex.Users.list_item_stockers client, "itemid"
+    Qiitex.Users.list_item_stockers client, "itemid", %{page: 1, per_page: 40}
+    ```
+  """
   def list_item_stockers(client, item_id) do
     get "/items/#{item_id}/stockers", client
   end
 
+  @doc """
+  List users in newest order.
+
+  ## example
+
+    ```
+    Qiitex.Users.list client
+    Qiitex.Users.list client, %{page: 1, per_page: 40}
+    ```
+  """
   def list(client) do
     get "/users", client
   end
 
+  @doc """
+  Get a user.
+
+  ## example
+
+    ```
+    Qiitex.Users.find client, "clientid"
+    ```
+  """
   def find(client, user_id) do
     get "/users/#{user_id}", client
   end
 
+  @doc """
+  List users a user is following
+
+  ## example
+
+    ```
+    Qiitex.Users.followees client, "userid"
+    Qiitex.Users.followees client, "userid", %{page: 1, per_page: 40}
+    ```
+  """
   def followees(client, user_id) do
     get "/users/#{user_id}/followees", client
   end
 
+  @doc """
+  List users who are following a user.
+
+  ## example
+
+    ```
+    Qiitex.Users.followers client, "userid"
+    Qiitex.Users.followers client, "userid", %{page: 1, per_page: 40}
+    ```
+  """
   def followers(client, user_id) do
     get "/users/#{user_id}/followers", client
   end
 
+  @doc """
+  Unfollow a user.
+
+  ## example
+
+    ```
+    Qiitex.Users.unfollow client, "userid"
+    ```
+  """
   def unfollow(client, user_id) do
     delete "/users/#{user_id}/following", client
   end
 
+  @doc """
+  Check if the current user is following a user.
+
+  ## example
+
+    ```
+    Qiitex.Users.get_follow client, "userid"
+    ```
+  """
   def get_follow(client, user_id) do
     get "/users/#{user_id}/following", client
   end
 
+  @doc """
+  Follow a user.
+
+  ## example
+
+    ```
+    Qiitex.Users.follow client, "userid"
+    ```
+  """
   def follow(client, user_id) do
     put "users/#{user_id}/following", client
   end
