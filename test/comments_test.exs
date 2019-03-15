39de3a49d798d9c14acd8293728a11d9df5ae530
tamanugi <tamanugi@gmail.com>
2019-03-16 00:24:08 +0900

    🚑 fix comments_test

--- test/comments_test.exs
+++ test/comments_test.exs
@@ -10,15 +10,18 @@ defmodule Qiitex.CommentsTest do
     HTTPoison.start
   end
 
-  test "find/2" do
+  test "not found comment" do
 
-    comment = Comment.get_comment(@client, "2dde107f6a0bcbf82edf")
-    assert ExJsonSchema.Validator.validate(@schema, comment) == :ok
+    {:error, error} = Comment.get_comment(@client, "hogehogehoge")
+    assert is_map error
+    assert error["message"] == "Not found"
   end
 
   test "list_item_comments/2" do
 
-    Comment.list_item_comments(@client, "5bbf6ef210273cf60dd4")
+    {:ok, comments} = Comment.list_item_comments(@client, "5bbf6ef210273cf60dd4")
+
+    comments
     |> Enum.each(fn(e) ->
       assert is_map e
       assert ExJsonSchema.Validator.validate(@schema , e) == :ok
